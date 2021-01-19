5754d2cfb8510fed831985b163cc0f920299ebc0
svdycl2858@gmail.com <svdycl2858@gmail.com>
2021-01-19 20:05:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string kurs1 = "yazılım geliştirici kampı";
+            //string kurs2 = "programlamaya başlangıç kursu";
+            //string kurs3 = "java kursu"; 
+            //aynı işlem dizi ile yapılırsa
+            string[] kurslar = new string[] { "yazılım geliştirici kampı", "programlamaya başlangıç kursu", "java kursu"};
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu-footer");
+        }
+    }
+}
